8b7dc1fa77725d72e05ff46455ed90fd218610bf
pranab1981 <pranab@gmail.com>
2015-11-05 18:55:02 +0530

    added logic to create unique sessions on the fly

--- client/home.js
+++ client/home.js
@@ -1,10 +1,45 @@
 /**
  * Created by Pranab Agarwal on 11/5/2015.
  */
-//
-//Template.home.onRendered(function(){
-//
-//});
+
+Template.home.onRendered(function(){
+
+    console.log("home.js: Inside onRendered");
+
+    //var sessionId = new Meteor.Collection.ObjectID()._str;
+
+    var collabIp = "100.100.100.100";
+    var collabName = "Test";
+    var collabColor = "Orange";
+
+    var createdAt = new Date();
+    var updatedAt = new Date();
+
+    var sessionId = DrawSessions.insert({
+        createdAt: createdAt,
+        updatedAt: updatedAt,
+        collabList:[{
+            collabIp:       collabIp,
+            collabName:     collabName,
+            collabColor:    collabColor
+        }]
+
+    },
+    function(error, result) {
+        if (error) {
+            console.log("Error:" + error.reason);
+            return;
+        }
+        else {
+            console.log("inside session callback");
+            console.log("/" + sessionId);
+            Router.go("/" + sessionId) ;
+        }
+    });
+
+
+
+});
 
 
 Template.home.events({
---
+++ server/publications.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Pranab Agarwal on 11/5/2015.
+ */
+//
+//Meteor.publish('sessionData',function() {
+//
+//    return Meteor.users.find();
+//
+//});
+
